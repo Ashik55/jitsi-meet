8527b7eec0d19212a5f79c6a91cdfc8ea096c621
Md. Mehedi Hasan <mehedi17n@gmail.com>
2025-07-24 13:50:32 +0600

    feat: replace text, add call audio and visual assets - audio file for call ringing sound - JSON lottie file for call animation layers - replace "meeting" with "call"

--- react/features/notifications/middleware.ts
+++ react/features/notifications/middleware.ts
@@ -178,11 +178,10 @@ MiddlewareRegistry.register(store => next => action => {
         const oldRole = oldParticipant?.role;
 
         if (oldRole && oldRole !== role && role === PARTICIPANT_ROLE.MODERATOR) {
-
-            store.dispatch(showNotification({
-                titleKey: 'notify.moderator'
-            },
-            NOTIFICATION_TIMEOUT_TYPE.SHORT));
+            // store.dispatch(showNotification({
+            //     titleKey: 'notify.moderator'
+            // },
+            // NOTIFICATION_TIMEOUT_TYPE.SHORT));
         }
 
         return next(action);
